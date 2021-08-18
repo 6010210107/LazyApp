e3dc4cce2121922074444c02b1c7a4b8f5c7e70c
Thanasorn <6010210107@psu.ac.th>
2021-08-18 12:43:49 +0700

    Change level cleaned code

--- src/components/sudokuTable/SudokuTable.js
+++ src/components/sudokuTable/SudokuTable.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import "./SudokuTable.css";
-import {
-  Button,
-  Form,
-  Row,
-  Col,
-  Dropdown,
-  DropdownButton,
-} from "react-bootstrap";
+import { Row, Col, Dropdown, DropdownButton } from "react-bootstrap";
 import { newStartingBoard, fillPuzzle } from "../../controller/sudoku/puzzle";
+
+/**
+ * ---------------------------------------------------------
+ *todo || Sudoku board.
+ *todo || Create the 9*9 board ✔
+ *todo || Generate the sudoku puzzle.  ✔
+ *todo || Fill the puzzle into the board.  ✔
+ *todo || Update value in field. [ ]
+ *todo || Create the information area. [ ]
+ *todo || Change level. ✔
+ * ----------------------------------------------------------
+ */
 
 const emptyPuzzle = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,13 +27,17 @@ const emptyPuzzle = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
+/**
+ * Main Component
+ */
 const SudokuTable = () => {
   const [difficulty, setDifficulty] = useState("Select Difficulty");
   let currentRef = useRef();
   let puzzleRef = useRef();
-
+  let difficultyList = ["Easy", "Medium", "Hard", "Expert"];
+  let holes = [20, 30, 40, 50];
   /**
-   * Check input is safe.
+   ** Check input is safe.
    */
   const isSafe = (row, col) => {
     let current = currentRef.current;
@@ -40,38 +49,10 @@ const SudokuTable = () => {
   };
 
   /**
-   * Generate the puzzle.
+   ** Update the puzzle board.
    */
-  // const beginPuzzle = async (holes = 20) => {
-  //   let current = currentRef.current
-  //   let puzzle = puzzleRef.current
-  //   let puzzle_assets = await generatePuzzle(holes);
-  //   puzzle = puzzle_assets;
-  //   current = JSON.parse(JSON.stringify(puzzle_assets[1]));
-  //   setSudokuBoard(renderTable);
-  //   return true;
-  // };
-
-  const initPuzzle = useCallback(async (holes) => {
-    let puzzle_assets = await generatePuzzle(holes);
-
-    puzzleRef.current = puzzle_assets;
-    currentRef.current = JSON.parse(JSON.stringify(puzzle_assets[1]));
-    setSudokuBoard(
-      <SudokuBoard
-        currentRef={currentRef}
-        puzzleRef={puzzleRef}
-        isSafe={isSafe}
-        updateBoard={updateBoard}
-      />
-    );
-  }, []);
-  /**
-   * Update the puzzle board.
-   */
-  const updateBoard = (position, targetVal) => {
+  const updateBoard = useCallback((position, targetVal) => {
     let current = currentRef.current;
-
     let newCurrent = current;
     let row = +position[0];
     let col = +position[1];
@@ -86,10 +67,31 @@ const SudokuTable = () => {
         updateBoard={updateBoard}
       />
     );
-  };
+  }, []);
 
+  /**
+   ** Generate the puzzle.
+   */
+  const initPuzzle = useCallback(
+    async (holes) => {
+      let puzzle_assets = await generatePuzzle(holes);
+      puzzleRef.current = puzzle_assets;
+      currentRef.current = JSON.parse(JSON.stringify(puzzle_assets[1]));
+      setSudokuBoard(
+        <SudokuBoard
+          currentRef={currentRef}
+          puzzleRef={puzzleRef}
+          isSafe={isSafe}
+          updateBoard={updateBoard}
+        />
+      );
+    },
+    [updateBoard]
+  );
 
-
+  /**
+   * Initial state.
+   */
   const [sudokuBoard, setSudokuBoard] = useState(
     <SudokuBoard
       currentRef={currentRef}
@@ -98,58 +100,27 @@ const SudokuTable = () => {
       updateBoard={updateBoard}
     />
   );
+
   useEffect(() => {
     initPuzzle();
   }, [initPuzzle]);
 
+  /**
+   * JSX return
+   */
   return (
     <div>
       <Row className="d-flex justify-content-around m-3 w-100">
-        <Col xs={12} md={4} className="d-flex flex-wrap">
-          <DropdownButton title={difficulty}>
-            <Dropdown.ItemText>Select Difficulty</Dropdown.ItemText>
-            {/* <Dropdown.Item as="button" onClick={() => beginPuzzle(81)}>
-              Empty Puzzle
-            </Dropdown.Item> */}
-            <Dropdown.Item
-              as="button"
-              onClick={() => {
-                setDifficulty("Easy");
-                initPuzzle(25);
-              }}
-            >
-              Easy
-            </Dropdown.Item>
-            <Dropdown.Item
-              as="button"
-              onClick={() => {
-                setDifficulty("Medium");
-                initPuzzle(40);
-              }}
-            >
-              Medium
-            </Dropdown.Item>
-            <Dropdown.Item
-              as="button"
-              onClick={() => {
-                setDifficulty("Hard");
-                initPuzzle(50);
-              }}
-            >
-              Hard
-            </Dropdown.Item>
-            <Dropdown.Item
-              as="button"
-              onClick={() => {
-                setDifficulty("Expert");
-                initPuzzle(60);
-              }}
-            >
-              Expert
-            </Dropdown.Item>
-          </DropdownButton>
+        <Col xs={12} md={4} className="d-flex flex-wrap pink mx-5">
+          <SudokuDropdown
+            setDifficulty={setDifficulty}
+            initPuzzle={initPuzzle}
+            difficulty={difficultyList}
+            holes={holes}
+            title={difficulty}
+          />
         </Col>
-        <Col xs={12} md={6} className="d-flex justify-content-center">
+        <Col xs={12} md={6} className="d-flex  px-5 justify-content-center ">
           <table>
             <tbody>{sudokuBoard}</tbody>
           </table>
@@ -161,6 +132,9 @@ const SudokuTable = () => {
 
 export default SudokuTable;
 
+/**
+ ** Generate the complete puzzle.
+ */
 const generatePuzzle = (difficulty = 20) => {
   const sudoku = newStartingBoard(difficulty);
 
@@ -168,9 +142,9 @@ const generatePuzzle = (difficulty = 20) => {
 };
 
 /**
- * Render sudoku board.
+ ** Render sudoku board.
  */
-const SudokuBoard = ({currentRef, puzzleRef, updateBoard, isSafe}) => {
+const SudokuBoard = ({ currentRef, puzzleRef, updateBoard, isSafe }) => {
   let current = currentRef.current;
   let puzzle = puzzleRef.current;
   return (
@@ -234,5 +208,50 @@ const SudokuBoard = ({currentRef, puzzleRef, updateBoard, isSafe}) => {
         ))}
       </tr>
     ))
+  );
+};
+
+/**
+ ** Dropdown
+ */
+const SudokuDropdown = ({
+  setDifficulty,
+  initPuzzle,
+  difficulty,
+  holes,
+  title,
+}) => {
+  return (
+    <DropdownButton title={title}>
+      <Dropdown.ItemText>Select Difficulty</Dropdown.ItemText>
+      {/* <Dropdown.Item as="button" onClick={() => beginPuzzle(81)}>
+              Empty Puzzle
+            </Dropdown.Item> */}
+      {difficulty.map((item, index) => {
+        return (
+          <DropdownItem
+            key={holes[index]}
+            setDifficulty={setDifficulty}
+            initPuzzle={initPuzzle}
+            difficulty={item}
+            holes={holes[index]}
+          />
+        );
+      })}
+    </DropdownButton>
+  );
+};
+
+const DropdownItem = ({ setDifficulty, initPuzzle, difficulty, holes }) => {
+  return (
+    <Dropdown.Item
+      as="button"
+      onClick={() => {
+        setDifficulty(difficulty);
+        initPuzzle(holes);
+      }}
+    >
+      {difficulty}
+    </Dropdown.Item>
   );
 };
